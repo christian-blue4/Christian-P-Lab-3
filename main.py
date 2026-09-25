8118c009a316ffd534495887b7526f74c7ce7d52
christian-blue4 <cparian29@setoncatholic.org>
2026-09-25 15:41:25 +0000

    final project + easter egg extra credit

--- main.py
+++ main.py
@@ -1,33 +1,50 @@
+def show_result(result):
+    print(result)
+    if result == 7082011:
+        print("Secret message: Happy birthday, Christian!")
+
 # this funtion adds two numbers,
 def add(x,y):
-    print(x + y)
-
-add(6,5)
-
+    show_result(x + y)
 # this funtion subtracts two numbers,
 def sub(x,y):
-    print(x - y)
-
-sub(6,5)
-
+    show_result(x - y)
 # this funtion multiplies two numbers,
 def mul(x,y):
-    print(x*y)
-
-mul(6,5)
-
+    show_result(x*y)
 # this funtion divides two numbers,
 def div(x,y):
-    print(x/y)
+    show_result(x/y)
 
-div(6,5)
+#################### start of program ####################
 
-###############################################
+print("Welcome to the calculator app.")
+print("What would you like to do?")
 
-x = int(input("Enter your first number: "))
-y = int(input("Enter your second number: "))
 
-add(x,y)
-sub(x,y)
-mul(x,y)
-div(x,y)
+#print(user_choice)
+
+while(True):
+    print("Type (a)dd, (s)ubtract, (m)ultiply, (d)ivide, or (q)uit")
+    user_choice = input(": ")
+    if user_choice == 'a':
+        x = int(input("Enter your first number: "))
+        y = int(input("Enter your second number: "))
+        add(x,y)
+    elif user_choice == 's':
+        x = int(input("Enter your first number: "))
+        y = int(input("Enter your second number: "))
+        sub(x,y)
+    elif user_choice == 'm':
+        x = int(input("Enter your first number: "))
+        y = int(input("Enter your second number: "))
+        mul(x,y)
+    elif user_choice == "d":
+        x = int(input("Enter your first number: "))
+        y = int(input("Enter your second number: "))
+        div(x,y)
+    elif user_choice == "q":
+        print("Closing program...")
+        break
+    else:
+        print("Are you stupid?")
